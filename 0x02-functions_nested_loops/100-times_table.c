2b547af63814c6f34b1ef8c1768bb4834de3e8f0
vikk-i <victoria_macadonai@hotmail.com>
2023-06-27 08:50:42 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,47 +1,22 @@
-#include "main.h"
-
+/*
+* File: 101-natural.c
+* Auth: vincent kip
+*/
+#include <stdio.h>
 /**
-  * print_times_table - prints the n times table, starting with 0
-  * @n: number of the times table
-  */
-void print_times_table(int n)
+* main - Lists all natural numbers below 1024 (excluded)
+* that are multiples of 3 0r 5.
+*
+* Return: Always 0.
+*/
+int main(void)
 {
-	int i, j, k;
-
-	if (n >= 0 && n <= 15)
-	{
-		for (i = 0; i <= n; i++)
-		{
-			for (j = 0; j <= n; j++)
-			{
-				k = j * i;
-				if (j == 0)
-				{
-					_putchar(k + '0');
-				} else if (k < 10 && j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				} else if (k >= 10 && k < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				} else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-			}
-			_putchar('\n');
-		}
-	}
+int i, sum = 0;
+for (i = 0; i < 1024; i++)
+{
+if ((i % 3) == 0 || (i % 5) == 0)
+sum += i;
+}
+printf("%d\n", sum);
+return (0);
 }
